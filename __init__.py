00fec655837323ec2bb2f159982b05411d9d3504
kft334 <93904307+kft334@users.noreply.github.com>
2024-01-08 07:31:26 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,14 +4,14 @@ from .Knodes import LoadImagesBase64
 
 NODE_CLASS_MAPPINGS = {
     "ImageOutput": ImageOutput,
-    "Load Image From Websocket (Base64)": LoadImageBase64,
-    "Load Images From Websocket (Base64)": LoadImagesBase64
+    "Load Image (Base64)": LoadImageBase64,
+    "Load Images (Base64)": LoadImagesBase64
     }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageOutput": "Image(s) To Websocket (Base64)",
-    "LoadImageBase64": "Load Image From Websocket (Base64)",
-    "LoadImagesBase64": "Load Images From Websocket (Base64)"
+    "LoadImageBase64": "Load Image (Base64)",
+    "LoadImagesBase64": "Load Images (Base64)"
     }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
